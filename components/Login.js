37250e539db5578088a4efcc6925fc3e02c98c17
mateus-ps <37370632+mateus-ps@users.noreply.github.com>
2018-10-22 16:10:46 -0300

    Create Login.js
    Implementei a tela Login.js
    Ainda falta complementar o Banco de Dados...

---
+++ components/Login.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Text, View, StyleSheet, AppRegistry, Image, Dimensions, TextInput, Button, TouchableOpacity, Alert } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const botaoPressionado = () => {
+  Alert.alert('Fazendo login...');
+};
+
+
+export default class Login extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image source={''} style={styles.logo}/>
+
+        <View>
+          <TextInput 
+            underlineColorAndroid='transparent' 
+            placeholder='Login' 
+            style={styles.textInput}
+          />
+        </View>
+
+         <View>
+          <TextInput underlineColorAndroid='transparent' placeholder='Senha' style={styles.textInput}/>
+        </View>
+
+        <View>
+          <TouchableOpacity onPress={botaoPressionado} style={styles.botao}>
+            <Text> ENTRAR </Text>
+          </TouchableOpacity>
+        </View>
+    </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: '#91b8da',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textInput:{
+    width: 300,
+    heigth: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    backgroundColor: 'white',
+    marginTop: 10
+  },
+  botao:{
+    backgroundColor: '#e88585',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    margintTop: 20
+  },
+  logo:{
+    width: 200,
+    heigth: 200,
+    margin: 20
+  },
+  
+});
